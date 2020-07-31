b8b75d4eacfff21ca4b4ff06187ef218e01d52c8
shumilina-polina <Shumilina246@gmail.com>
2020-07-31 13:38:37 +0300

    create: newsletter send and footer form send

---
+++ send.php
@@ -0,0 +1,62 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$email = $_POST['email'];
+
+// Формирование самого письма
+$title = "Новое обращение Best Tour Plan";
+$body = "
+<h2>Новое обращение</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone<br><br>
+<b>Сообщение:</b><br>$message
+<b>Почта:</b><br>$email
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'Secondaryshum@gmail.com'; // Логин на почте
+    $mail->Password   = 'MNq-Fe9g'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('Secondaryshum@gmail.com', 'Полина Шумилина'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('Shumilina246@gmail.com');  
+    // $mail->addAddress('youremail@gmail.com'); 
+    // Ещё один, если нужен
+
+    
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;    
+
+    // Проверяем отравленность сообщения
+    if ($mail->send()) {$result = "success";} 
+    else {$result = "error";}
+
+    } catch (Exception $e) {
+        $result = "error";
+        $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+    }
+
+    // Отображение результата
+    // echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
+    header('Location: thankyou.html');
